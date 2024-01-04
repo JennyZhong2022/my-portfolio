59c726d98addc5c676928ad65d1c92591ab4a8d1
Xiao Zhong <zhongxiao2017@gmail.com>
2024-01-04 13:19:39 +1100

    navbar done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+
+    </section>
   )
 }
 
