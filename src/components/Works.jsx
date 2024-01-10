f74be603fc860c0c9eb4cafb0ea71616de003a8e
Xiao Zhong <zhongxiao2017@gmail.com>
2024-01-11 01:19:55 +1100

    works trail on mobile update

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -58,14 +58,16 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link,w
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <motion.div
+        // variants={textVariant()}
+      >
       <p className={styles.sectionSubText}>My Work</p>
         <h2 className={styles.sectionHeadText}>Project.</h2>
       </motion.div>
 
       <div className='w-full flex'>
         <motion.p
-          variants={fadeIn('', '', 0.1, 1)}
+          // variants={fadeIn('', '', 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
           Following projects showcases my skills and experience through real-world examples of my work.Each one was finished in one week. Click the card to go the project website, click the github sign to go github repo.  
