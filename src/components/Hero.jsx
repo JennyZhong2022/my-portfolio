974061e4b337049d5d48fadae551e414991450d1
Xiao Zhong <zhongxiao2017@gmail.com>
2024-01-12 22:54:37 +1100

    update Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,33 @@
 import { motion } from 'framer-motion'
 import { styles } from "../styles"
-// import { ComputersCanvas } from './canvas'
+import { ComputersCanvas } from './canvas'
+import {  useEffect, useState } from "react";
+
 
 
 
 const Hero = () => {
+  const [isMobile, setIsMobile] = useState(false)
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width:500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const _handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", _handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", _handleMediaQueryChange);
+    };
+  }, []);
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
@@ -17,14 +40,16 @@ const Hero = () => {
                  Jenny </span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-         I take immense pride in what I craft and the innovations I bring to life. In the future, I aspire to produce even more, developing creations that can positively impact and transform people&apos;s lives. I am excited about opportunities that allow me to grow, learn, and contribute to meaningful and creative projects in the tech world.
+            I take immense pride in what I craft and the innovations I bring to life. In the future, I aspire to produce even more, developing creations that can positively impact and transform people&apos;s lives.
+            {isMobile? <p className={`${styles.heroSubText} mt-2 text-white-100`} >I am excited about opportunities that allow me to grow, learn, and contribute to meaningful and creative projects in the tech world.</p>:<p></p>}
+         
           </p>
         </div>
       </div>
       
    
   
-        {/* <ComputersCanvas /> */}
+       {!isMobile?<ComputersCanvas /> : null}
      
     
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
